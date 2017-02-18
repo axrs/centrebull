de0d4d03f787151870ab08e36b3424ac39db4c36
Caleb Macdonald Black <calebmacdonaldblack@gmail.com>
2017-02-19 09:48:39 +1000

    added db migration with suggest column and trigger

---
+++ resources/migrations/20170219091344-shooters-suggest-trigger.up.sql
@@ -0,0 +1,24 @@
+--Add suggest column to shooters
+ALTER TABLE shooters
+  ADD COLUMN suggest TEXT,
+  ADD COLUMN updated_at TIMESTAMPTZ DEFAULT now();
+
+--Function to update the shooters suggestion field
+CREATE OR REPLACE FUNCTION update_shooters()
+  RETURNS TRIGGER AS $$
+BEGIN
+  NEW.suggest := NEW.sid || ' ' || NEW.last_name || ' ' || NEW.first_name || ' ' || NEW.preferred_name || ' ' ||
+                 NEW.club;
+  NEW.updated_at := now();
+  RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+--Trigger to update the user suggestion field
+CREATE TRIGGER update_shooters_trg
+BEFORE INSERT OR UPDATE ON shooters
+FOR EACH ROW EXECUTE PROCEDURE update_shooters();
+
+--Run trigger on all existing rows
+UPDATE shooters
+SET first_name = first_name;
