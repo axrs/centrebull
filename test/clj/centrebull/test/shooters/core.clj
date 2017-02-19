03ebfe315372911ad935ba3fced7e2d474e690a8
Caleb Macdonald Black <calebmacdonaldblack@gmail.com>
2017-02-19 11:37:20 +1000

    test suggest function extracts search query

--- test/clj/centrebull/test/shooters/core.clj
+++ test/clj/centrebull/test/shooters/core.clj
@@ -24,3 +24,11 @@
   (testing "prepare-shooter-search-terms"
     (is (= (#'centrebull.db.shooters/prepare-shooter-search-terms "Johnny Search Term")
            ["%johnny%" "%search%" "%term%"]))))
+
+(deftest suggest
+  (testing "suggest"
+    (let [rv (gen-shooter) es "Johnny Search Term"]
+      (with-redefs [dao/suggest (mock-dao/suggest es rv)]
+        (let [{:keys [status body]} (shooters/suggest {:query-params {"q" es}})]
+          (is (= body (gen-shooter)))
+          (is (= status 200)))))))
