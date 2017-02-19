3a699b9fb865d64818ead7e23e85571571c48b0b
Caleb Macdonald Black <calebmacdonaldblack@gmail.com>
2017-02-19 12:52:08 +1000

    moved test inside if shooters test scope

--- test/clj/centrebull/test/shooters/core.clj
+++ test/clj/centrebull/test/shooters/core.clj
@@ -18,17 +18,16 @@
       (with-redefs [dao/create! (mock-dao/create! expected nil)]
         (let [{:keys [status body]} (shooters/create! {:body-params expected})]
           (is (= body nil))
+          (is (= status 200))))))
+
+  (testing "shooter-suggest"
+    (let [rv (gen-shooter) es "Johnny Search Term"]
+      (with-redefs [dao/suggest (mock-dao/suggest es rv)]
+        (let [{:keys [status body]} (shooters/suggest {:query-params {"q" es}})]
+          (is (= body (gen-shooter)))
           (is (= status 200)))))))
 
 (deftest prepare-shooter-search-terms
   (testing "prepare-shooter-search-terms"
     (is (= (#'centrebull.db.shooters/prepare-shooter-search-terms "Johnny Search Term")
            ["%Johnny%" "%Search%" "%Term%"]))))
-
-(deftest suggest
-  (testing "suggest"
-    (let [rv (gen-shooter) es "Johnny Search Term"]
-      (with-redefs [dao/suggest (mock-dao/suggest es rv)]
-        (let [{:keys [status body]} (shooters/suggest {:query-params {"q" es}})]
-          (is (= body (gen-shooter)))
-          (is (= status 200)))))))
