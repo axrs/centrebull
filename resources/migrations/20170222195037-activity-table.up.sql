8057693acf852ad8b9b1101e2674ddf6f33d1b9f
Alexander Scott <xander@cadcoder.com>
2017-02-22 20:08:27 +1000

    UUID gen and timestamp

--- resources/migrations/20170222195037-activity-table.up.sql
+++ resources/migrations/20170222195037-activity-table.up.sql
@@ -1,7 +1,7 @@
 CREATE TABLE activities (
-  id             UUID PRIMARY KEY,
+  id             UUID PRIMARY KEY NOT NULL DEFAULT gen_random_uuid(),
   competition_id UUID REFERENCES competitions (id),
   range_id       UUID REFERENCES ranges (id),
-  priority       INT DEFAULT 1,
-  date           DATE NOT NULL
+  priority       INT                       DEFAULT 1,
+  date           TIMESTAMP        NOT NULL
 )
