cf9850f3feff8d55d7a60733272dca9165ee5781
Lachlan Robertson <lachlan.robertson142@gmail.com>
2017-02-22 21:49:10 +1000

    Added :name to correctly reference sql query in clojure

--- resources/sql/shooters.sql
+++ resources/sql/shooters.sql
@@ -13,7 +13,7 @@ WHERE /*~ (string/join " AND " (for [value params] (str "suggest ILIKE '" value 
 ORDER BY sid ASC, first_name ASC, last_name ASC
 LIMIT 25;
 
--- :shooters-findById :? :1
+-- :name shooters-find-by-id :? :*
 -- :doc Finds a shooter by an id
 SELECT *
 FROM shooters
