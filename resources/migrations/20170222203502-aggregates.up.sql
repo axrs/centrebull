27b7097d1c329b6694ef7eeff902377d0d93f987
calebmacdonaldblack <calebmacdonaldblack@gmail.com>
2017-02-22 21:06:02 +1000

    added formatting

--- resources/migrations/20170222203502-aggregates.up.sql
+++ resources/migrations/20170222203502-aggregates.up.sql
@@ -1,5 +1,5 @@
 CREATE TABLE aggregates (
-  id UUID PRIMARY KEY NOT NULL DEFAULT gen_random_uuid(),
-  description TEXT NOT NULL,
+  id          UUID PRIMARY KEY NOT NULL DEFAULT gen_random_uuid(),
+  description TEXT             NOT NULL,
   activities  UUID REFERENCES activities (id)
 )
