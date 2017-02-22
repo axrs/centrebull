8f2fa15a2f63c46725b36d3b12121a2778a06629
Lachlan Robertson <lachlan.robertson142@gmail.com>
2017-02-22 20:55:47 +1000

    Changed shooter_id to be more inline with other tables

--- resources/migrations/20170222200628-results-table.up.sql
+++ resources/migrations/20170222200628-results-table.up.sql
@@ -1,6 +1,6 @@
 CREATE TABLE results (
     id UUID PRIMARY KEY NOT NULL DEFAULT gen_random_uuid(),
-    shooter_id NUMERIC REFERENCES shooters (sid),
+    sid NUMERIC REFERENCES shooters (sid),
     activity_id UUID REFERENCES activities (id),
     shots TEXT NOT NULL,
     score INT NOT NULL,
