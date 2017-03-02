a260d35e5f4fcec53d11a1deb88db4b1ae1aab89
calebmacdonaldblack <calebmacdonaldblack@gmail.com>
2017-03-02 16:32:00 +1000

    added ranges delete func

--- src/clj/centrebull/ranges/core.clj
+++ src/clj/centrebull/ranges/core.clj
@@ -4,3 +4,9 @@
 
 (defn create! [{:keys [all-params]}]
   (response/ok (dao/create! all-params)))
+
+(defn delete! [{:keys [all-params]}]
+  (->> all-params
+    :range/id
+    dao/delete!
+    response/ok))
