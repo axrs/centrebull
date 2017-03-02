d1e511caf4381fcfb3a94213f5d782b66a8f479f
calebmacdonaldblack <calebmacdonaldblack@gmail.com>
2017-03-02 17:49:46 +1000

    added outmapper

--- src/clj/centrebull/db/ranges.clj
+++ src/clj/centrebull/db/ranges.clj
@@ -3,11 +3,16 @@
             [centrebull.db.util :refer [mapper]]
             [centrebull.db.core :refer [ranges-create!]]))
 
-(def ^:private key-map {:range/description :description})
+(def ^:private key-map {:range/description :description
+                        :range/id :id})
 
+(def ^:private value-map (map-invert key-map))
+
+(defn- out-mapper [m] (mapper value-map m))
 (defn- in-mapper [m] (mapper key-map m))
 
 (defn create! [range]
   (->> range
     in-mapper
-    ranges-create!))
+    ranges-create!
+    out-mapper))
