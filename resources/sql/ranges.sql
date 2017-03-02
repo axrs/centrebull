cdeca301685cb65188d74db5180b3ee5d1c104ef
calebmacdonaldblack <calebmacdonaldblack@gmail.com>
2017-03-02 17:47:47 +1000

    modified sql to return resource

--- resources/sql/ranges.sql
+++ resources/sql/ranges.sql
@@ -1,5 +1,6 @@
--- :name ranges-create! :! :n
+-- :name ranges-create! :<! :1
 -- :doc create a range record
 INSERT INTO ranges
 (description)
-VALUES (:description);
+VALUES (:description)
+RETURNING *;
