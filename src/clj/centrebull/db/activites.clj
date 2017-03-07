9e483d95f4544b9cb56cb95f897b5c4a791dc1e6
calebmacdonaldblack <calebmacdonaldblack@gmail.com>
2017-03-07 19:55:26 +1000

    added activities sql and dao

---
+++ src/clj/centrebull/db/activites.clj
@@ -0,0 +1,21 @@
+(ns centrebull.db.activites
+  (:require [clojure.set :refer [map-invert]]
+            [centrebull.db.util :refer [mapper]]
+            [centrebull.db.core :refer [activity-create!]]))
+
+(def ^:private key-map {:activity/id             :id
+                        :activity/competition-id :competition/id
+                        :activity/range-id       :range-id
+                        :activity/priority       :priority
+                        :activity/date           :date})
+
+(def ^:private value-map (map-invert key-map))
+
+(defn- out-mapper [m] (mapper value-map m))
+(defn- in-mapper [m] (mapper key-map m))
+
+(defn create! [activity]
+  (->> activity
+    in-mapper
+    activity-create!
+    out-mapper))
