50475ccbd8c84879e72deca55ff72980a09ff51d
Lachlan Robertson <lachlan.robertson142@gmail.com>
2017-03-08 20:46:15 +1000

    Entries dao with basic setup and create! function

---
+++ src/clj/centrebull/db/entries.clj
@@ -0,0 +1,21 @@
+(ns centrebull.db.entries
+  (:require [clojure.string :refer [split lower-case]]
+            [clojure.set :refer [map-invert]]
+            [centrebull.db.util :refer [mapper]]
+            [centrebull.db.core :refer [entries-create]]))
+
+
+
+(def ^:private key-map {:competition/id          :id
+                        :shooter/id              :sid
+                        :shooter/grade           :grade})
+
+(def ^:private value-map (map-invert key-map))
+
+(defn- out-mapper [m] (mapper value-map m))
+(defn- in-mapper [m] (mapper key-map m))
+
+(defn register! [competition]
+  (->> competition
+    in-mapper
+    entries-create))
