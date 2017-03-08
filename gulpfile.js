6176629aa910c98eddb5153dd9a462646b8eaca3
Alexander Scott <xander@cadcoder.com>
2017-03-08 18:49:32 +1000

    Update Gulp Builder

--- gulpfile.js
+++ gulpfile.js
@@ -2,12 +2,24 @@
 require('events').EventEmitter.prototype._maxListeners = 100;
 
 var gulp = require('gulp');
-var watch = require('gulp-watch-less');
+var gutil = require('gulp-util');
+var notify = require('gulp-notify');
 var less = require('gulp-less');
+var autoprefix = require('gulp-autoprefixer');
+var minifyCSS = require('gulp-minify-css');
 
-gulp.task('default', function () {
-    return gulp.src('src/less/bare.less')
-        .pipe(watch('src/less/*.less'))
-        .pipe(less())
-        .pipe(gulp.dest('resources/public/css'))
+var lessDir = 'src/less';
+var targetCSSDir = 'public/css';
+
+gulp.task('css', function () {
+    return gulp.src(lessDir + '/semantic.less')
+        .pipe(less({style: 'compressed'}).on('error', gutil.log))
+        .pipe(gulp.dest(targetCSSDir))
+        .pipe(notify('CSS minified'))
 });
+
+gulp.task('watch', function () {
+    gulp.watch(lessDir + '/*.less', ['css']);
+});
+
+gulp.task('default', ['css', 'watch']);
