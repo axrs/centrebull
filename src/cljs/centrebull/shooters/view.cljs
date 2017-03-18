e3bd0819ada3c58d9df2364cfec5bdbb61b5051a
calebmacdonaldblack <calebmacdonaldblack@gmail.com>
2017-03-18 16:16:24 +1000

    added search for shooters

--- src/cljs/centrebull/shooters/view.cljs
+++ src/cljs/centrebull/shooters/view.cljs
@@ -1,7 +1,14 @@
-(ns centrebull.shooters.view)
+(ns centrebull.shooters.view
+  (:require [centrebull.components.search :refer [search]]
+            [re-frame.core :as rf]))
 
 (defn shooters-page []
   [:section
    [:card
-    [:h2 {:local "9/12"} "Shooters"]]])
-
+    [:h2 {:local "9/12"} "Shooters"]
+    [search "/shooters/search"
+     (fn [{:keys [shooter/sid shooter/preferred-name shooter/first-name shooter/last-name shooter/club]}]
+       [:div {:on-click #(rf/dispatch [:set-active-shooter sid])}
+        [:div {:local "1/3"} sid]
+        [:div {:local "1/3"} (or preferred-name (str first-name " " last-name))]
+        [:div {:local "1/3"} club]])]]])
