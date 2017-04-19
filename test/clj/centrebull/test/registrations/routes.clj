453023d8708e3401c6f9c7d9779df37c3636c6b5
calebmacdonaldblack <calebmacdonaldblack@gmail.com>
2017-04-19 23:46:06 +1000

    improved test accuracy

--- test/clj/centrebull/test/registrations/routes.clj
+++ test/clj/centrebull/test/registrations/routes.clj
@@ -13,23 +13,22 @@
 (use-fixtures :once wrap-test)
 
 (deftest registrations-routes
-  (let [response-body {:response-body "called"}]
     (testing "Register Shooter Route"
-      (with-redefs [registrations/register-shooter! (constantly (response/ok response-body))]
+      (with-redefs [registrations/register-shooter! (constantly (response/ok {:register-shooter "called"}))]
         (let [{:keys [status body]} ((app) (json-request :post "/registrations" (gen-entry)))]
           (is (= status 200))
-          (is (= response-body (parse-body body))))))
+          (is (= {:register-shooter "called"} (parse-body body))))))
 
     (testing "Unregister shooter route"
-      (with-redefs [registrations/unregister-shooter! (constantly (response/ok response-body))]
+      (with-redefs [registrations/unregister-shooter! (constantly (response/ok {:unregister-shooter "called"}))]
         (let [{:keys [status body]} ((app) (json-request :delete (str "/registrations/" (uuid))))]
           (is (= status 200))
-          (is (= response-body (parse-body body))))))
+          (is (= {:unregister-shooter "called"} (parse-body body))))))
 
     (testing "Search Registrations"
-      (with-redefs [registrations/suggest-registration (constantly (response/ok response-body))]
+      (with-redefs [registrations/suggest-registration (constantly (response/ok {:suggest-registration "called"}))]
         (let [{:keys [status body]} ((app) (json-request :post "/registrations/search"
                                                          {:search/q "asdf"
                                                           :competition/id (uuid)}))]
           (is (= status 200))
-          (is (= response-body (parse-body body))))))))
+          (is (= {:suggest-registration "called"} (parse-body body)))))))
