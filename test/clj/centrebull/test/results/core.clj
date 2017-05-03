f7af337a79659031f3ecac9d8791d836117640bb
calebmacdonaldblack <calebmacdonaldblack@gmail.com>
2017-05-03 21:39:07 +1000

    add test for conflict

--- test/clj/centrebull/test/results/core.clj
+++ test/clj/centrebull/test/results/core.clj
@@ -1,6 +1,7 @@
 (ns centrebull.test.results.core
   (:require [clojure.test :refer :all]
             [centrebull.test.mock-generators :refer :all]
+            [centrebull.test.util :refer [do-not-call]]
             [centrebull.db.results :as dao]
             [centrebull.test.db.results :as mock-dao]
             [centrebull.results.core :as results]))
@@ -8,7 +9,16 @@
 (deftest results
   (testing "Results-Create!"
     (let [expected (gen-result)]
-      (with-redefs [dao/create! (mock-dao/create! expected expected)]
+      (with-redefs [dao/create! (mock-dao/create! expected expected)
+                    dao/exists? (constantly false)]
         (let [{:keys [status body]} (results/create! {:all-params expected})]
           (is (= body expected))
-          (is (= status 200)))))))
+          (is (= status 200))))))
+
+  (testing "Results-Create! conflict"
+    (let [result (gen-result)]
+      (with-redefs [dao/create! do-not-call
+                    dao/exists? (constantly true)]
+        (let [{:keys [status body]} (results/create! {:all-params result})]
+          (is (= body {:errors {:conflict "Results for shooter on this activity already exist!"}}))
+          (is (= status 409)))))))
