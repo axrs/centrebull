cf52d97c8a3eb71e4031471211eb168bb402ce1d
Alexander Scott <xander@cadcoder.com>
2017-05-03 23:31:54 +1000

    Competition Modal

--- src/cljs/centrebull/competitions/core.cljs
+++ src/cljs/centrebull/competitions/core.cljs
@@ -1,6 +1,7 @@
 (ns centrebull.competitions.core
   (:require
     [secretary.core :as secretary]
+    [centrebull.components.modal :as modal]
     [centrebull.competitions.handlers]
     [centrebull.competitions.views :as v]
     [re-frame.core :as rf]
@@ -9,16 +10,17 @@
 
 (defn- page []
   (let [new-competition (r/atom {})
-        errors (r/atom {})
-        show-modal? (r/atom false)
-        toggle-action #(rf/dispatch [:toggle show-modal? new-competition errors])
-        submit-action #(rf/dispatch [:competitions-create @new-competition errors [[:toggle show-modal? new-competition errors]]])
+        toggle-action #(modal/toggle new-competition)
+        reset-action #(modal/reset new-competition)
+        submit-action #(rf/dispatch [:competitions-create @new-competition reset-action])
         valid? (fn [] (s/valid? :api/competition-create @new-competition))]
     (fn []
       [:div
-       (v/competitions-page toggle-action)
-       (when @show-modal?
-         (v/register-modal new-competition valid? toggle-action submit-action))])))
+       [v/competitions-page toggle-action]
+
+       [modal/modal {:state new-competition
+                     :title "Register New Competition"
+                     :view  [v/register new-competition valid? toggle-action submit-action]}]])))
 
 (secretary/defroute "/competitions" []
   (rf/dispatch [:set-active-page :competitions]))
