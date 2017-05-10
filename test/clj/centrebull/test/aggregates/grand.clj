2868c47c57c31a24b13b6acb9b5ed5e48cdfad34
Alexander Scott <xander@cadcoder.com>
2017-05-10 22:57:59 +1000

    Tests

---
+++ test/clj/centrebull/test/aggregates/grand.clj
@@ -0,0 +1,51 @@
+(ns centrebull.test.aggregates.grand
+  (:require
+    [clojure.test :refer :all]
+    [centrebull.aggregates.grand :as grand]
+    [centrebull.db.grand-aggregates :as dao]
+    [centrebull.test.db.grand-aggregates :as mock-dao]
+    [centrebull.db.aggregates :as aggregates-dao]
+    [centrebull.test.db.aggregates :as aggregates-mock-dao]
+    [centrebull.test.util :refer [uuid]]
+    [centrebull.test.mock-generators :refer :all]))
+
+(deftest grand-aggregates
+  (testing "Aggregates-Create!"
+    (let [expected (gen-aggregate)]
+      (with-redefs [dao/create! (mock-dao/create! expected nil)
+                    aggregates-dao/find-for-competition-and-in-coll (aggregates-mock-dao/find-for-competition-and-in-coll expected (:aggregate/activities expected))]
+        (let [{:keys [status body]} (grand/create! {:all-params expected})]
+          (is (= body nil))
+          (is (= status 200))))))
+
+  (testing "Aggregates-Create! with invalid activity ids"
+    (let [expected (gen-aggregate)]
+      (with-redefs [dao/create! (mock-dao/create! expected nil)
+                    aggregates-dao/find-for-competition-and-in-coll (aggregates-mock-dao/find-for-competition-and-in-coll expected [])]
+        (let [{:keys [status body]} (grand/create! {:all-params expected})]
+          (is (= body {:errors {:aggregate/activities "Not all activies found in competition."}}))
+          (is (= status 400))))))
+
+  (testing "Aggregates-Find"
+    (let [expected {:competition/id (uuid)}
+          aggregates [(gen-grand-aggregate)]]
+      (with-redefs [dao/find (mock-dao/find expected aggregates)]
+        (let [{:keys [status body]} (grand/find-aggregates {:all-params expected})]
+          (is (= body aggregates))
+          (is (= status 200))))))
+
+  (testing "Aggregates-Find-Results"
+    (let [expected {:competition/id (uuid) :activity/id (uuid)}
+          aggregates [(gen-aggregate-result)]]
+      (with-redefs [dao/find-results (mock-dao/find-results expected aggregates)]
+        (let [{:keys [status body]} (grand/find-aggregate-results {:all-params expected})]
+          (is (= body aggregates))
+          (is (= status 200))))))
+
+  (testing "Aggregates-Delete!"
+    (let [all-params {:aggregate/id (uuid) :competition/id (uuid)}]
+      (with-redefs [dao/delete-aggregate! (mock-dao/delete-aggregate! all-params nil)]
+        (let [{:keys [status body]} (grand/delete-aggregate! {:all-params all-params})]
+          (is (= body nil))
+          (is (= status 200)))))))
+
---
+++ test/clj/centrebull/test/db/grand_aggregates.clj
@@ -0,0 +1,32 @@
+(ns centrebull.test.db.grand-aggregates
+  (:require [clojure.test :refer :all]))
+
+(defn create!
+  [em rv]
+  (fn [aggregate]
+    (is (= em aggregate))
+    rv))
+
+(defn find
+  [em rv]
+  (fn [competition-id]
+    (is (= em competition-id))
+    rv))
+
+(defn find-by-id
+  [em rv]
+  (fn [param-map]
+    (is (= em param-map))
+    rv))
+
+(defn delete-aggregate!
+  [em rv]
+  (fn [id-map]
+    (is (= em id-map))
+    rv))
+
+(defn find-results
+  [em rv]
+  (fn [id-map]
+    (is (= em id-map))
+    rv))
