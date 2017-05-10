3196da36d03f3d1c1a8067c7306f54cc6a6534a3
calebmacdonaldblack <calebmacdonaldblack@gmail.com>
2017-05-10 23:14:39 +1000

    Created the base of the grand aggregate page

--- src/cljs/centrebull/grand-aggregates/core.cljs
+++ src/cljs/centrebull/grand-aggregates/core.cljs
@@ -1,9 +1,39 @@
 (ns centrebull.grand-aggregates.core
   (:require [secretary.core :as secretary]
-            [re-frame.core :as rf]))
+            [re-frame.core :as rf]
+            [reagent.core :as r]
+            [centrebull.grand-aggregates.views :as v]
+            [clojure.spec :as s]))
 
 (defn- page []
-  [:h1 "grand aggregates"])
+  (let [activites (rf/subscribe [:activities])
+        competition-id @(rf/subscribe [:active-competition-id])
+        aggregated (r/atom {:competition/id        competition-id
+                            :activities            []
+                            :aggregate/activities  []
+                            :aggregate/priority    1
+                            :aggregate/description ""})
+        ;; CHANGE BELOW
+        reset-action #(reset! aggregated {:compeition/id competition-id :activities []})
+        ;; CHANGE BELOW
+        submit #(rf/dispatch [:aggregate-create @aggregated [:refresh-aggregates] reset-action])
+        ;; CHANGE BELOW
+        toggle-action (fn [res add?]
+                        (if add?
+                          (->>
+                            (into (:activities @aggregated) [res])
+                            distinct
+                            (sort-by :activity/priority)
+                            (swap! aggregated assoc :activities))
+                          (->> @aggregated
+                            :activities
+                            (filter (partial not= res))
+                            (sort-by :activity/priority)
+                            (swap! aggregated assoc :activities)))
+                        (swap! aggregated assoc :aggregate/activities (mapv :activity/id (:activities @aggregated))))
+        ;; CHANGE BELOW
+        valid? (fn [] (s/valid? :api/aggregate-create @aggregated))]
+    (v/grand-aggregates-page activites toggle-action submit valid? aggregated)))
 
 (def pages
   {:grand-aggregates #'page})
--- src/cljs/centrebull/grand-aggregates/views.cljs
+++ src/cljs/centrebull/grand-aggregates/views.cljs
@@ -1,1 +1,50 @@
-(ns centrebull.grand-aggregates.views)
+(ns centrebull.grand-aggregates.views
+  (:require [centrebull.aggregates.views :refer [aggregate-section aggregate-form]]
+            [centrebull.date-utils :refer [format-date]]))
+
+(defn aggregate-table-head []
+  [:thead
+   [:tr
+    [:th "#"]
+    [:th "Description"]
+    [:th "Actions"]]])
+
+(defn activity-row
+  [submit {:keys [activity/id
+                  activity/date
+                  range/description
+                  activity/priority] :as act} add?]
+  [:tr
+   [:td priority]
+   [:td description]
+   [:td (format-date date)]
+   [:td [:button {:on-click #(submit act add?)} (if add? "Add" "Remove")]]])
+
+(defn grand-aggregates-page [aggregates action submit valid? aggregated]
+  [:div
+    [:section
+      [:card
+        [:h2 {:local "9/12"} "Grand Aggregates"]]]
+    [aggregate-section]
+    [:section
+      [:card
+        [:h4 "Create new grand aggregate"]
+        [aggregate-form aggregated]
+        [:table
+          [aggregate-table-head]
+          [:tbody]
+          (for [act (:activities @aggregated)]
+            ^{:key (:activity/id act)} [activity-row action act false])]
+        [:button {:data-pull-left "9/12"
+                  :local          "3/12"
+                  :data-m-full    ""
+                  :data-primary   ""
+                  :on-click       submit
+                  :disabled       (not (valid?))} "Submit"]]]
+    [:section
+     [:card
+      [:table
+       [aggregate-table-head
+        [:tbody
+         (for [aggregate aggregates]
+          [activity-row action aggregate true])]]]]]])
