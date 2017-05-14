4a75e8f4ea82a1399d9bd03c1338afb3a0f54f58
calebmacdonaldblack <calebmacdonaldblack@gmail.com>
2017-05-14 11:55:48 +1000

    rank wip

--- src/cljs/centrebull/aggregates/handlers.cljs
+++ src/cljs/centrebull/aggregates/handlers.cljs
@@ -90,11 +90,15 @@
   (fn [{:keys [db]} [_ results]]
     (let [h (find-max-priorities results)
           pred (update-shooter h)
-          grouped (group-by :shooter/sid results)]
-
-      {:db (assoc db :active-aggregate-results (->> grouped
-                                                 (map pred)
-                                                 (sort-by :sort-key >)))})))
+          grouped (group-by :shooter/sid results)
+          results (->> grouped
+                    (map pred)
+                    (sort-by :sort-key >))
+          grouped-results (group-by :shooter/grade results)
+          ranked-grouped-results (map (fn [[grade group]] (map-indexed #(assoc %2 :rank (inc %1)) group)) grouped-results)
+          merged-ranked-grouped-results (apply concat ranked-grouped-results)
+          b (cljs.pprint/pprint merged-ranked-grouped-results)]
+      {:db (assoc db :active-aggregate-results ranked-grouped-results)})))
 
 (reg-event-fx
   :refresh-aggregate-results
