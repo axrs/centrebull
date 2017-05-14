32f4d9c788ae86fe1d563ac9da537b99adb19048
calebmacdonaldblack <calebmacdonaldblack@gmail.com>
2017-05-14 12:46:22 +1000

    added docs

--- src/cljs/centrebull/utils.cljs
+++ src/cljs/centrebull/utils.cljs
@@ -1,6 +1,8 @@
 (ns centrebull.utils)
 
-(defn rank-results [results]
+(defn rank-results
+  "Ranks results based on grade"
+  [results]
   (let [grouped-results (group-by :shooter/grade results)
         ranked-grouped-results (map (fn [[grade group]] (map-indexed #(assoc %2 :rank (inc %1)) group)) grouped-results)
         merged-ranked-grouped-results (apply concat ranked-grouped-results)]
