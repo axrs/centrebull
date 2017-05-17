f2d0fa29edb77735eae254244ca0ef882d5d187c
calebmacdonaldblack <calebmacdonaldblack@gmail.com>
2017-05-17 19:07:27 +1000

    refactored view to views

--- src/cljs/centrebull/display/core.cljs
+++ src/cljs/centrebull/display/core.cljs
@@ -3,43 +3,12 @@
     [secretary.core :as secretary]
     [re-frame.core :as rf]
     [reagent.core :as r]
-    [centrebull.display.views :as views]))
+    [centrebull.display.views :as v]))
 
 (defn set-timeout []
   (js/setInterval #(rf/dispatch [:update-tv]) (* 5 1000)))
 
 (defonce timeout (set-timeout))
-
-(defn grade-limit [grade]
-  (case grade
-    "A"   10
-    "B"   8
-    "C"   5
-    "FO"  5
-    "FTR" 5
-    "FSA" 6
-    "FSB" 6))
-  
-
-(defn agg-row [pri aggs {:keys [rank shooter/grade shooter/first-name shooter/last-name shooter/club shooter/sid aggregate/results aggregate/score aggregate/vs]}]
-  [:tr
-   [:td rank]
-   [:td first-name " " last-name]
-   (for [r pri]
-     (let [res (first (filter #(= r (:aggregate/priority %)) results))]
-       (if (some #{(:aggregate/priority res)} aggs)
-         ^{:key (str "card-row-" sid r)} [:tdh (or (:result/score res) [:code]) [:sup (:result/vs res)]]
-         ^{:key (str "card-row-" sid r)} [:td (or (:result/score res) [:code]) [:sup (:result/vs res)]])))])
-
-(defn- grade-card [grouped-results pri aggs]
-  (for [grade (keys grouped-results)]
-    ^{:key (str "grade-card-" grade)}
-    [:tbody
-     [:tr [:td {:col-span "50"} "Grade: " grade]]
-     (let [results (get grouped-results grade)
-           limited-results (take (grade-limit grade) results)]
-      (for [s limited-results]
-        ^{:key (:shooter/sid s)} [agg-row pri aggs s]))]))
 
 (defn- page []
   (let [results @(rf/subscribe [:tv-results])
@@ -49,32 +18,7 @@
         left-col (dissoc grouped-results "A" "B" "C")
         f (first results)
         pri (apply sorted-set (into #{} (map :aggregate/priority (:aggregate/results f))))]
-    [:section
-     [:tv
-      [:div {:local "1/2"}
-       [:card
-        [:table
-         [:thead
-          [:tr
-           [:th "#"]
-           [:th "Name"]
-           (for [r pri]
-             (if (some #{r} aggregate-priorities)
-               ^{:key (str "grand-agg" r)} [:thh "#" r]
-               ^{:key (str "grand-agg" r)} [:th "#" r]))]]
-         (grade-card right-col pri aggregate-priorities)]]]
-      [:div {:local "1/2"}
-       [:card
-        [:table
-         [:thead
-          [:tr
-           [:th "#"]
-           [:th "Name"]
-           (for [r pri]
-             (if (some #{r} aggregate-priorities)
-               ^{:key (str "grand-agg" r)} [:thh "#" r]
-               ^{:key (str "grand-agg" r)} [:th "#" r]))]]
-         (grade-card left-col pri aggregate-priorities)]]]]]))
+    [v/display-page results aggregate-priorities grouped-results right-col left-col pri]))
 
 (secretary/defroute "/tv" []
   (rf/dispatch [:bull-clicked]))
